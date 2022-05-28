310ab783bcd44722169901528c79b3a43a673474
wwcx <85999717+wwcxin@users.noreply.github.com>
2022-05-28 14:59:44 +0800

    Update copy.js

--- js/copy.js
+++ js/copy.js
@@ -15,34 +15,34 @@
 			}
 		}
 
-		//function loop() {
-			//var startTime = new Date();
-			//debugger;
-			//if (new Date() - startTime > timeLimit) {
-				//if (!open) {
-					//callbacks.forEach(function(fn) {
-						//fn.call(null);
-					//});
-				//}
-				//open = true;
-				//window.stop();
-				//alert('CNM温馨提醒：\n小样！还想扒我的站？\n自己老实一点！');
-				//document.body.innerHTML = "";
-			//} else {
-				//open = false;
-			//}
-		//}
-	//})()).addListener(function() {
-		//window.location.reload();
-	//});
+		function loop() {
+			var startTime = new Date();
+			debugger;
+			if (new Date() - startTime > timeLimit) {
+				if (!open) {
+					callbacks.forEach(function(fn) {
+						fn.call(null);
+					});
+				}
+				open = true;
+				window.stop();
+				alert('CNM温馨提醒：\n小样！还想扒我的站？\n自己老实一点！');
+				document.body.innerHTML = "";
+			} else {
+				open = false;
+			}
+		}
+	})()).addListener(function() {
+		window.location.reload();
+	});
 
 // js键盘监听事件-监听键盘F12(123)-触发文件为空
-//$(document).keydown(function(event) {
-//	if (event.keyCode == 123) {
-//		alert('CNM温馨提醒：\n小伙子！你想干嘛？又想扒我站？');
-//		document.body.innerHTML = "";
-//	}
-//});
+$(document).keydown(function(event) {
+	if (event.keyCode == 123) {
+		alert('CNM温馨提醒：\n小伙子！你想干嘛？又想扒我站？');
+		document.body.innerHTML = "";
+	}
+});
 
 //显示版权
 //console.log("\n %c CNM.SB %c https://cnm.sb/ \n\n",
